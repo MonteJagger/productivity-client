683e9574b20d9f9d86fb0073a43a1ec9bc6ead81
hiumathy <hiumathy.learn@gmail.com>
2020-05-16 18:28:18 -0400

    add material to the add-item form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +17,7 @@ import { ItemComponent } from './components/item/item.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 @NgModule({
   declarations: [
@@ -28,7 +34,13 @@ import { AddItemComponent } from './components/add-item/add-item.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
